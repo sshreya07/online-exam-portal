d55ddaceffb545b41069229028bafc7dd230239e
sshreya07 <gshreya0799@gmail.com>
2020-10-15 22:30:22 +0530

    updated

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -1,12 +1,22 @@
+import { Card, CardContent } from '@material-ui/core'
 import React from 'react'
 
 const LeftNav = () => {
     return (
         <div className="leftpanel">
             <ul>
-                <li>Dashboard</li>
-                <li>Student</li>
-                <li>Teacher</li>
+                <li><Card><CardContent>Dashboard</CardContent></Card></li>
+            </ul>
+            <ul>Student
+                <li><Card><CardContent>Attend Exams</CardContent></Card></li>
+                <li><Card><CardContent>Profile</CardContent></Card></li>
+            </ul>
+            <ul>Teacher
+                <li><Card><CardContent>Schedule Exams</CardContent></Card></li>
+                <li><Card><CardContent>Results</CardContent></Card></li>
+            </ul>
+            <ul>Admin
+                <li><Card><CardContent>Account</CardContent></Card></li>
             </ul>
         </div>
     )
