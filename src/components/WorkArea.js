5e6351fb67c71c50a10e1611847323d18cac6ba8
sshreya07 <gshreya0799@gmail.com>
2020-10-25 14:30:08 +0530

    updated

--- src/components/WorkArea.js
+++ src/components/WorkArea.js
@@ -11,85 +11,78 @@ import {
 import SwipeableViews from 'react-swipeable-views';
 
 const WorkArea = () => {
-    function TabPanel(props) {
-        const { children, value, index, ...other } = props;
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
       
-        return (
-          <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`full-width-tabpanel-${index}`}
-            aria-labelledby={`full-width-tab-${index}`}
-            {...other}
-          >
-            {value === index && (
-              <Box p={3}>
-                <Typography>{children}</Typography>
-              </Box>
-            )}
-          </div>
-        );
-      }
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`full-width-tabpanel-${index}`}aria-labelledby={`full-width-tab-${index}`} {...other} >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+   );
+  }
 
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          flexGrow: 1,
-        },
-        padding: {
-          padding: theme.spacing(3),
-        },
-        demo2: {
-          backgroundColor: '#1f4068',
-          height:'auto',
-          borderRadius:'10px'
-        },
-      }));
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    padding: {
+      padding: theme.spacing(3),
+    },
+    demo2: {
+      backgroundColor: '#1f4068',
+      height:'auto',
+      borderRadius:'10px'
+    },
+  }));
 
-    const StyledTabs = withStyles({
-        indicator: {
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: 'transparent',
-          '& > span': {
-            maxWidth: 60,
-            width: '100%',
-            backgroundColor: 'white',
-          },
-        },
-      })((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+  const StyledTabs = withStyles({
+    indicator: {
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: 'transparent',
+      '& > span': {
+        maxWidth: 60,
+        width: '100%',
+        backgroundColor: 'white',
+      },
+    },
+  })((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
       
-      const StyledTab = withStyles((theme) => ({
-        root: {
-          textTransform: 'none',
-          color: '#fff',
-          fontWeight: theme.typography.fontWeightRegular,
-          fontSize: theme.typography.pxToRem(15),
-          marginRight: theme.spacing(1),
-          '&:focus': {
-            opacity: 1,
-          },
-        },
-      }))((props) => <Tab disableRipple {...props} />);
+  const StyledTab = withStyles((theme) => ({
+    root: {
+      textTransform: 'none',
+      color: '#fff',
+      fontWeight: theme.typography.fontWeightRegular,
+      fontSize: theme.typography.pxToRem(15),
+      marginRight: theme.spacing(1),
+      '&:focus': {
+       opacity: 1,
+      },
+    },
+  }))((props) => <Tab disableRipple {...props} />);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-      };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
     
-      const handleChangeIndex = (index) => {
-        setValue(index);
-      };
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
       
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-    function a11yProps(index) {
-        return {
-          id: `full-width-tab-${index}`,
-          'aria-controls': `full-width-tabpanel-${index}`,
-        };
-      }
+  function a11yProps(index) {
+    return {
+      id: `full-width-tab-${index}`, 'aria-controls': `full-width-tabpanel-${index}`,
+    };
+  }
 
     return (
         <div className="workarea">
