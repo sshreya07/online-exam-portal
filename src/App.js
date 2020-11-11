ec2646586a62125690e67be8d85d7bd2a350235d
sshreya07 <gshreya0799@gmail.com>
2020-11-11 21:06:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 import ExamState from './components/context/ExamState';
-import LeftNav from './components/LeftNav';
-import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from './components/Dashboard';
 import Exams from './components/Exams';
 import Profile from './components/Profile';
 import CreateQues from './components/CreateQues';
 import ScheduleExam from './components/ScheduleExam';
+import Home from './components/Home';
+import {Link} from 'react-router-dom';
+import {Button} from '@material-ui/core';
 
 const App = () => {
   return (
     <ExamState>
       <Router>
         <div className="App">
-          <Navbar/>
-          <LeftNav/>
+          <Link to="/home"><Button>HOME</Button></Link>
           <Switch>
+            <Route exact path="/home" component={Home} />
             <Route exact path="/dashboard" component={Dashboard}/>
             <Route exact path="/exams" component={Exams} />
             <Route exact path="/profile" component={Profile} />
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LeftNav from './LeftNav';
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+        <div>
+          <Navbar/>
+          <LeftNav/>
+        </div>
+    )
+}
+
+export default Home
