83aa9a74daeab568b71b394bd3898439d202f230
sshreya07 <gshreya0799@gmail.com>
2020-11-16 18:25:46 +0530

    updated

--- src/components/BoardingPage.js
+++ src/components/BoardingPage.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Button, Card, CardContent} from '@material-ui/core';
-import BackgroundImage from './backgroundNew.jpg';
+import {Button} from '@material-ui/core';
+import BackgroundImage from './iconNew.jpg';
 
 
 
 const BoardingPage = () => {
     return (
         <div className="onboard">
-           <Card id="outerCard">
-             <CardContent>
-             <ul>
-              <li> <Link to="/home" style={{textDecoration:'none'}}><Button variant="outlined">Teacher</Button></Link>
+            <ul>
+              <li> <Link to="/home" style={{textDecoration:'none', fontWeight:'lighter'}}><Button variant="outlined">ABOUT</Button></Link>
               </li>
               <li>
-              <Link to="/home" style={{textDecoration:'none'}}><Button variant="outlined">Student</Button></Link>
+              <Link to="/home" style={{textDecoration:'none', fontWeight:'lighter'}}><Button variant="outlined">CONTACT US</Button></Link>
               </li>
             </ul>
-            <Card className="innerCard">
-              <CardContent>
-                <img src={BackgroundImage} alt="online exam" />
-              </CardContent>
-            </Card> 
-             </CardContent>
-           </Card>
+            <div className="text" >
+              <h1>Online<br/> Exam Platform<br/> 
+                <ul style={{position:'relative', right:'11rem', top:'3rem'}}>
+                  <li> <Link to="/home" style={{textDecoration:'none'}}><Button variant="outlined">GET STARTED</Button></Link>
+                  </li>
+                </ul>
+              </h1>
+             
+            </div>
+            <div style={{margin:'1.8rem 0 0 5.5rem',display:'flex',justifyContent:'flex-end'}}>
+              <img src={BackgroundImage} alt="online exam" />
+            </div>
+                
            <div className="footer">
             <ul>
-              <li> Build with React
+              <li> 
               </li>
             </ul>
             </div>
